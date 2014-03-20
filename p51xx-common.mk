10130af2c69c517270dd255796ea7e34deb5f9c8
Andreas B <skate4life@gmx.de>
2014-03-20 01:11:23 +0100

    P51XX: Add TWRP config
    Change-Id: Iaeddfb8ae5a89c984990107145fe9968ac407cfd

--- p51xx-common.mk
+++ p51xx-common.mk
@@ -35,7 +35,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.espresso10.usb.rc:root/init.espresso10.usb.rc \
     $(LOCAL_PATH)/rootdir/init.espresso10.rc:root/init.espresso10.rc \
     $(LOCAL_PATH)/rootdir/ueventd.espresso10.rc:root/ueventd.espresso10.rc \
-    $(LOCAL_PATH)/rootdir/fstab.espresso10:root/fstab.espresso10
+    $(LOCAL_PATH)/rootdir/fstab.espresso10:root/fstab.espresso10 \
+    $(LOCAL_PATH)/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab
 
 # Audio
 PRODUCT_COPY_FILES += \
