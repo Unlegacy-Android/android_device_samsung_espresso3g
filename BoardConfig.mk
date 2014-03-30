c10c70d467c61ab089955a9532ca558c48557b9b
Andreas B <skate4life@gmx.de>
2014-03-30 23:01:56 +0200

    P5100: Use SlimRoms espresso10 kernel-source
    Since CM updated his cm-11.0 espresso10 source some P51XX faceing
    a big battery drain (happens on different roms using this source).
    This problems seems to be related to "apply P3110 JB Update1 from
    opensource.samsung.com", not happens to all user.
    
    Now we pulled the espresso10 kernel source to SlimRoms git.
    The mentioned commit got reverted now in our source to solve the
    battery drain.
    
    Change-Id: I69c6f10d4d6c605043b82eec53e764ee2a0c7f0d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ TARGET_BOARD_INFO_FILE := device/samsung/p5100/board-info.txt
 
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/samsung/espresso10
-TARGET_KERNEL_CONFIG := cyanogenmod_p5100_defconfig
+TARGET_KERNEL_CONFIG := slim_p5100_defconfig
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := p5100,GT-P5100,espresso10rf,espresso10rfxx
