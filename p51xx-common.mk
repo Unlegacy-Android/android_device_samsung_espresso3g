b7f52b629f8e12f986e571b35fb93f220bf218db
Andreas Blaesius <skate4life@gmx.de>
2015-07-16 21:53:04 +0200

    P51XX: cleanup / rework ramdisk parts for better readability
    Change-Id: Ic5ea906ccbc348a4201628ac3269f5500e7830f7

--- p51xx-common.mk
+++ p51xx-common.mk
@@ -30,13 +30,16 @@ LOCAL_PATH := device/samsung/p5100
 PRODUCT_AAPT_CONFIG := normal large tvdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := tvdpi
 
-# Init files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.espresso10.usb.rc:root/init.espresso10.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.espresso10.rc:root/init.espresso10.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.espresso10.rc:root/ueventd.espresso10.rc \
-    $(LOCAL_PATH)/rootdir/fstab.espresso10:root/fstab.espresso10 \
-    $(LOCAL_PATH)/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.espresso10 \
+    init.espresso10.usb.rc \
+    init.espresso10.rc \
+    ueventd.espresso10.rc
+
+# Recovery Ramdisk
+PRODUCT_PACKAGES += \
+    twrp.fstab
 
 # Audio
 PRODUCT_COPY_FILES += \
