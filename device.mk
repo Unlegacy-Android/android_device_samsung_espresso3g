15fc339d1f29022a001b0133943b9663e51517bf
Ziyan <jaraidaniel@gmail.com>
2016-01-29 12:10:05 +0100

    audio: select correct audio config at runtime (4/5)
    Change-Id: Iddd27bd39ad978257d0c83a0aefd43073ec2cf62

--- device.mk
+++ device.mk
@@ -24,7 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso-common/overlay/aosp-p51xx
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/espresso10rf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Packages
