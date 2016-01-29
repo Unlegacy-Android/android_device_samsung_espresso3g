a8858370f0c6ac0ed989aad904b0eca39a1b9b5c
Ziyan <jaraidaniel@gmail.com>
2016-01-29 12:10:05 +0100

    audio: select correct audio config at runtime (4/5)

--- device.mk
+++ device.mk
@@ -23,7 +23,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/espresso10rf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # RIL
