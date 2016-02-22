5fa5e4030f3413e455799da270834004395e8a85
Andreas Blaesius <skate4life@gmx.de>
2016-02-23 00:34:44 +0100

    espresso: move RIL parts to espresso3g (2/2)
    Change-Id: I7b4bcfad2967b6cbfcce43c0a8612bdaf7aa9e42

--- device.mk
+++ device.mk
@@ -30,8 +30,12 @@ PRODUCT_PACKAGES += \
     SamsungServiceMode
 
 # RIL
+PRODUCT_PACKAGES += \
+    libsecril-client
+
 PRODUCT_PROPERTY_OVERRIDES += \
-    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0
+    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
+    ro.telephony.ril_class=SamsungOmap4RIL
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
