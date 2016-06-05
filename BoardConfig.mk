754677028f35cdc9c533c8d4e222e27e9157073d
Andreas Blaesius <skate4life@gmx.de>
2016-06-05 20:43:59 +0200

    Update TARGET_OTA_ASSERT_DEVICE
    - cleanup BoardConfig.mk
    
    Change-Id: I72e248f2916eef9e29658a709aa62b8fea8904f8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ include device/samsung/espresso/BoardConfigCommon.mk
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/espresso3g/include
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := espresso,p5100,GT-P5100,espresso10rf,espresso10rfxx,p3100,GT-P3100,espressorf,espressorfxx
+TARGET_OTA_ASSERT_DEVICE := espresso3g,espresso-common,p3100,GT-P3100,espressorf,espressorfxx,p5100,GT-P5100,espresso10rf,espresso10rfxx
 
 # RIL
 BOARD_PROVIDES_LIBRIL := true
