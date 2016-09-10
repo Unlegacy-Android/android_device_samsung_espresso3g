206fb7a76953b0683f597d54012334eab7293e11
Ziyan <jaraidaniel@gmail.com>
2016-09-10 09:36:38 +0200

    Revert "espresso-common: Disable ashmem tracking in parcels"
    This reverts commit 825f8108082519b168774046be15a67ba41cc553.
    
    Conflicts:
    	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,6 @@ include device/samsung/espresso/BoardConfigCommon.mk
 # assert
 TARGET_OTA_ASSERT_DEVICE := espresso3g,espresso-common,p3100,GT-P3100,espressorf,espressorfxx,p5100,GT-P5100,espresso10rf,espresso10rfxx
 
-# RIL
-COMMON_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
-
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     device/samsung/espresso3g/sepolicy
